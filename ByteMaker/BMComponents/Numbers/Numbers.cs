5389ce5e8ef7a55d71d1b8cfac301497e8188965
Annie Locke <alocke@lockesoftware.co.uk>
2022-12-17 05:31:01 +0000

    The Number components are actually written correctly... probably

--- ByteMaker/BMComponents/Numbers/Numbers.cs
+++ ByteMaker/BMComponents/Numbers/Numbers.cs
@@ -24,18 +24,38 @@ public class BMSignedByte: BMFileComponent
 
 public class BMShort: BMFileComponent
 {
-    public override byte[] Write(object content) { return BitConverter.GetBytes((short)content); }
+    public override byte[] Write(object content)
+    {
+        byte[] raw = BitConverter.GetBytes((short)content);
+        byte[] actual = new byte[2];
+        for (int i = 0; i < raw.Length; i++)
+        {
+            actual[2 - raw.Length - 1 + i] = raw[i];
+        }
+
+        return actual;
+    }
 
     public override object Read(ref int index, ref byte[] readBytes)
-    { index += 2; return BitConverter.ToInt32(readBytes[index..(index + 4)]); }
+    { index += 2; return BitConverter.ToInt32(readBytes[index..(index + 2)]); }
 }
 
 public class BMUnsignedShort: BMFileComponent
 {
-    public override byte[] Write(object content) { return BitConverter.GetBytes((ushort)content); }
+    public override byte[] Write(object content)
+    {
+        byte[] raw = BitConverter.GetBytes((ushort)content);
+        byte[] actual = new byte[2];
+        for (int i = 0; i < raw.Length; i++)
+        {
+            actual[2 - raw.Length - 1 + i] = raw[i];
+        }
+
+        return actual;
+    }
 
     public override object Read(ref int index, ref byte[] readBytes)
-    { index += 2; return BitConverter.ToUInt16(readBytes[index..(index + 4)]); }
+    { index += 2; return BitConverter.ToUInt16(readBytes[index..(index + 2)]); }
 }
 
 #endregion
@@ -44,7 +64,17 @@ public class BMUnsignedShort: BMFileComponent
 
 public class BMInteger: BMFileComponent
 {
-    public override byte[] Write(object content) { return BitConverter.GetBytes((int)content); }
+    public override byte[] Write(object content)
+    {
+        byte[] raw = BitConverter.GetBytes((int)content);
+        byte[] actual = new byte[4];
+        for (int i = 0; i < raw.Length; i++)
+        {
+            actual[4 - raw.Length - 1 + i] = raw[i];
+        }
+
+        return actual;
+    }
 
     public override object Read(ref int index, ref byte[] readBytes)
     { index += 4; return BitConverter.ToInt32(readBytes[index..(index + 4)]); }
@@ -52,7 +82,17 @@ public class BMInteger: BMFileComponent
 
 public class BMUnsignedInteger: BMFileComponent
 {
-    public override byte[] Write(object content) { return BitConverter.GetBytes((uint)content); }
+    public override byte[] Write(object content)
+    {
+        byte[] raw = BitConverter.GetBytes((uint)content);
+        byte[] actual = new byte[4];
+        for (int i = 0; i < raw.Length; i++)
+        {
+            actual[4 - raw.Length - 1 + i] = raw[i];
+        }
+
+        return actual;
+    }
 
     public override object Read(ref int index, ref byte[] readBytes)
     { index += 4; return BitConverter.ToUInt32(readBytes[index..(index + 4)]); }
@@ -64,7 +104,16 @@ public class BMUnsignedInteger: BMFileComponent
 
 public class BMLong: BMFileComponent
 {
-    public override byte[] Write(object content) { return BitConverter.GetBytes((long)content); }
+    public override byte[] Write(object content)
+    {
+        byte[] raw = BitConverter.GetBytes((long)content);
+        byte[] actual = new byte[16];
+        for (int i = 0; i < raw.Length; i++)
+        {
+            actual[16 - raw.Length - 1 + i] = raw[i];
+        }
+        return actual;
+    }
 
     public override object Read(ref int index, ref byte[] readBytes)
     { index += 16; return BitConverter.ToInt64(readBytes[index..(index + 16)]); }
@@ -72,7 +121,16 @@ public class BMLong: BMFileComponent
 
 public class BMUnsignedLong: BMFileComponent
 {
-    public override byte[] Write(object content) { return BitConverter.GetBytes((ulong)content); }
+    public override byte[] Write(object content)
+    {
+        byte[] raw = BitConverter.GetBytes((ulong)content);
+        byte[] actual = new byte[16];
+        for (int i = 0; i < raw.Length; i++)
+        {
+            actual[16 - raw.Length - 1 + i] = raw[i];
+        }
+        return actual;
+    }
 
     public override object Read(ref int index, ref byte[] readBytes)
     { index += 16; return BitConverter.ToUInt64(readBytes[index..(index + 16)]); }
