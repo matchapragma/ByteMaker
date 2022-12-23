cd90c69f44ca5dd097eb35d85ccaca726a250663
Annie Locke <alocke@lockesoftware.co.uk>
2022-12-23 14:38:42 +0000

    Added pragmas for BMLazyString.cs

--- ByteMaker/BMComponents/String/BMLazyString.cs
+++ ByteMaker/BMComponents/String/BMLazyString.cs
@@ -41,7 +41,9 @@ namespace ByteMaker
             byte[] stringBytes = encoding switch
             {
                 BMTextEncoding.ASCII => Encoding.ASCII.GetBytes(contents),
+                #if NET5_0_OR_GREATER
                 BMTextEncoding.Latin1 => Encoding.Latin1.GetBytes(contents),
+                #endif
                 BMTextEncoding.Unicode => Encoding.Unicode.GetBytes(contents),
                 _ => throw new ArgumentOutOfRangeException()
             };
@@ -80,7 +82,9 @@ namespace ByteMaker
             return encoding switch
             {
                 BMTextEncoding.ASCII => Encoding.ASCII.GetString(thisString.ToArray()),
+                #if NET5_0_OR_GREATER
                 BMTextEncoding.Latin1 => Encoding.Latin1.GetString(thisString.ToArray()),
+                #endif
                 BMTextEncoding.Unicode => Encoding.Unicode.GetString(thisString.ToArray()),
                 _ => throw new ArgumentOutOfRangeException()
             };
